cc8d17efc119a024f04537169916e336f24f521d
Jakub Pajor <jay.pajor@gmail.com>
2019-12-07 20:05:20 +0100

    [master] heroku no test runner added

--- music_app_backend/settings.py
+++ music_app_backend/settings.py
@@ -126,8 +126,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-django_heroku.settings(locals())
-
+# Configure Django App for Heroku.
+django_heroku.settings(locals(), test_runner=False)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
